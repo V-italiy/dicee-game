f58d8e9fac0191a5c151232ce205c40a966a096e
V-italiy <108426033+V-italiy@users.noreply.github.com>
2023-07-10 17:44:24 -0400

    Update index.js
    Player name change

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ var image2 = document
   .setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩Anastasia Wins!";
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+  document.querySelector("h1").innerHTML = "Matthew Wins!🚩";
 } else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
